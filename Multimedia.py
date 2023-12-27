21c61ef6a3fa7458578677317fc742817b948c29
BinaryBen1 <Seeburg3@gmail.com>
2023-12-27 17:45:00 +0100

    Updates plotting
    cleaner plotting in code and visualization

--- Multimedia.py
+++ Multimedia.py
@@ -7,9 +7,7 @@ class Node:
         self.freq = freq
         self.left = None
         self.right = None
-    
 
-    # Funktion zur Erstellung der Buchstaben-Häufigkeits-Tabelle
 def generate_frequency_table(text):
     frequency = {}
     for char in text:
@@ -17,80 +15,87 @@ def generate_frequency_table(text):
             frequency[char] += 1
         else:
             frequency[char] = 1
-    return dict(sorted(frequency.items(), key=lambda x:x[1],reverse=True))
-
+    return dict(sorted(frequency.items(), key=lambda x: x[1], reverse=True))
 
 def generate_huffman_tree():
-    print('Bauen Baumstruktur vom frequency table: ',frequency_table)
     Knoten = [Node(char, freq) for char, freq in frequency_table.items()]
     
-    while len(Knoten)>1:
-        left=Knoten.pop()
-        right=Knoten.pop()
-        merged = Node(left.char+right.char, left.freq + right.freq)
+    while len(Knoten) > 1:
+        left = Knoten.pop()
+        right = Knoten.pop()
+        merged = Node(left.char + right.char, left.freq + right.freq)
         merged.left = left
         merged.right = right
-        if len(Knoten)==0:
-            Knoten.append(merged)
-            break
+        if len(Knoten) == 0:
+            return merged
         for index, x in enumerate(Knoten):
-            if x.freq<=merged.freq:
+            if x.freq <= merged.freq:
                 Knoten.insert(index, merged)
                 break
-    print("generated huffman tree")
-    return Knoten[0]
 
-def plot_huffman_tree(node, x=0, y=0, ax=None, depth=0, node_counts=None):
+def plot_tree_3d(node, x=0, y=0, z=0, ax=None, depth=1, branch_length=1, node_counts=None):
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
 
     if node.char is not None:
         text_node = node.char + ':' + str(node.freq)
-        ax.text(x, y, depth, text_node)
+        ax.text(x, y, z + 0.1, text_node)
 
     if node_counts is None:
         node_counts = {}
 
     if depth not in node_counts:
-        node_counts[depth] = 0
+        node_counts[depth - 1] = 0
 
-    if node.left:
-        node_counts[depth + 1] = node_counts.get(depth + 1, 0)
-        x_left = x - (2 ** (depth + 1 - 1))
-        ax.plot([x, x_left], [y, y + 1], [depth, depth + 1], marker='o')
-        ax.text((x + x_left) / 2, (y + (y + 1)) / 2, (depth + (depth + 1)) / 2, '0')
-        plot_huffman_tree(node.left, x_left, y + 1, ax, depth + 1, node_counts)
+    if node is not None:
+        ax.scatter(x, y, z, color='black')
 
-    if node.right:
-        node_counts[depth + 1] = node_counts.get(depth + 1, 0)
-        x_right = x + (2 ** (depth + 1 - 1))
-        ax.plot([x, x_right], [y, y + 1], [depth, depth + 1], marker='o')
-        ax.text((x + x_right) / 2, (y + (y + 1)) / 2, (depth + (depth + 1)) / 2, '1')
-        plot_huffman_tree(node.right, x_right, y + 1, ax, depth + 1, node_counts)
+        if node.left is not None:
+            node_counts[depth + 1] = node_counts.get(depth + 1, 0)
+            left_x = x - 1 / (2 ** depth)  # Adjusting x-coordinate based on depth
+            left_y = y - 1 / (2 ** depth)  # Adjusting y-coordinate based on depth
+            left_z = z - 1 / (2 ** depth)  # Adjusting z-coordinate based on depth
+            ax.plot([x, left_x], [y, left_y], [z, left_z], color='blue')
+            ax.text((x + left_x) / 2, (y + left_y) / 2, (z + left_z) / 2 + 0.1, '0', color='blue')
+            plot_tree_3d(node.left, left_x, left_y, left_z, ax, depth + 1, branch_length, node_counts)
+
+        if node.right is not None:
+            node_counts[depth + 1] = node_counts.get(depth + 1, 0)
+            right_x = x + 1 / (2 ** depth)  # Adjusting x-coordinate based on depth
+            right_y = y - 1 / (2 ** depth)  # Adjusting y-coordinate based on depth
+            right_z = z - 1 / (2 ** depth)  # Adjusting z-coordinate based on depth
+            ax.plot([x, right_x], [y, right_y], [z, right_z], color='red')
+            ax.text((x + right_x) / 2, (y + right_y) / 2, (z + right_z) / 2 + 0.1, '1', color='red')
+            plot_tree_3d(node.right, right_x, right_y, right_z, ax, depth + 1, branch_length, node_counts)
 
     return ax
 
-
-#problem nodes colliden
-
-# Beispieltext als Eingabe
-text = "MISSISSIXXI"                        #für 4 unterschiedliche Buchstaben gehts nicht, liegt wahrscheinlich am plot
+# Huffman Code algorithm
+text = "MISSISSIPPI"
+frequency_table = generate_frequency_table(text)
+root = generate_huffman_tree()
 
 fig = plt.figure(figsize=(10, 5))
+plt.title('3D Binary Tree Visualization')
 
-# Buchstaben-Häufigkeits-Tabelle erstellen und anzeigen
-frequency_table = generate_frequency_table(text)
+# Add subplot for the 3D tree visualization
+ax1 = fig.add_subplot(121, projection='3d')
+
+# Plot the Huffman tree in 3D
+ax1 = plot_tree_3d(root, ax=ax1)
+
+# Add subplot for the frequency table
 ax2 = fig.add_subplot(122)
 ax2.axis('off')
 ax2.table(cellText=[[char, freq] for char, freq in frequency_table.items()],
           colLabels=['Buchstabe', 'Häufigkeit'],
           loc='center')
-#
-#Huffman Baum erstellen und anzeigen
-ax1 = fig.add_subplot(121, projection='3d')
-root = generate_huffman_tree()
 
-
-plot_huffman_tree(root, ax=ax1)
 plt.show()
+
+
+"""
+singe character words do not work
+MISSISSIPPI does not work 4 different characters
+"""
