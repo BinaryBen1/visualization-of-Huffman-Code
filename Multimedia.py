5493bbd373e102104ed8bda8cebbe1146df9c3bc
BinaryBen1 <Seeburg3@gmail.com>
2023-12-27 18:09:39 +0100

    fixed Bugs
    all character lengths can be proceed now

--- Multimedia.py
+++ Multimedia.py
@@ -9,6 +9,8 @@ class Node:
         self.right = None
 
 def generate_frequency_table(text):
+    if not text:
+        raise Exception("The Input-String is empty.")
     frequency = {}
     for char in text:
         if char in frequency:
@@ -21,17 +23,15 @@ def generate_huffman_tree():
     Knoten = [Node(char, freq) for char, freq in frequency_table.items()]
     
     while len(Knoten) > 1:
+        Knoten = sorted(Knoten, key=lambda x: x.freq, reverse=True)
         left = Knoten.pop()
-        right = Knoten.pop()
-        merged = Node(left.char + right.char, left.freq + right.freq)
+        right = Knoten.pop() if len(Knoten) > 0 else None
+        merged = Node(left.char + (right.char if right else ''), left.freq + (right.freq if right else 0))
         merged.left = left
         merged.right = right
-        if len(Knoten) == 0:
-            return merged
-        for index, x in enumerate(Knoten):
-            if x.freq <= merged.freq:
-                Knoten.insert(index, merged)
-                break
+        Knoten.append(merged)
+
+    return Knoten[0] if Knoten else None
 
 def plot_tree_3d(node, x=0, y=0, z=0, ax=None, depth=1, branch_length=1, node_counts=None):
     if ax is None:
@@ -40,7 +40,7 @@ def plot_tree_3d(node, x=0, y=0, z=0, ax=None, depth=1, branch_length=1, node_co
 
     if node.char is not None:
         text_node = node.char + ':' + str(node.freq)
-        ax.text(x, y, z + 0.1, text_node)
+        ax.text(x, y, z + 0.03, text_node)
 
     if node_counts is None:
         node_counts = {}
@@ -53,39 +53,38 @@ def plot_tree_3d(node, x=0, y=0, z=0, ax=None, depth=1, branch_length=1, node_co
 
         if node.left is not None:
             node_counts[depth + 1] = node_counts.get(depth + 1, 0)
-            left_x = x - 1 / (2 ** depth)  # Adjusting x-coordinate based on depth
-            left_y = y - 1 / (2 ** depth)  # Adjusting y-coordinate based on depth
-            left_z = z - 1 / (2 ** depth)  # Adjusting z-coordinate based on depth
+            left_x = x - 1 / (2 ** depth)  
+            left_y = y - 1 / (2 ** depth)  
+            left_z = z - 1 / (2 ** depth)  
             ax.plot([x, left_x], [y, left_y], [z, left_z], color='blue')
-            ax.text((x + left_x) / 2, (y + left_y) / 2, (z + left_z) / 2 + 0.1, '0', color='blue')
+            ax.text((x + left_x) / 2, (y + left_y) / 2, (z + left_z) / 2 + 0.03, '0', color='blue')
             plot_tree_3d(node.left, left_x, left_y, left_z, ax, depth + 1, branch_length, node_counts)
 
         if node.right is not None:
             node_counts[depth + 1] = node_counts.get(depth + 1, 0)
-            right_x = x + 1 / (2 ** depth)  # Adjusting x-coordinate based on depth
-            right_y = y - 1 / (2 ** depth)  # Adjusting y-coordinate based on depth
-            right_z = z - 1 / (2 ** depth)  # Adjusting z-coordinate based on depth
+            right_x = x + 1 / (2 ** depth)
+            right_y = y - 1 / (2 ** depth)  
+            right_z = z - 1 / (2 ** depth)  
             ax.plot([x, right_x], [y, right_y], [z, right_z], color='red')
-            ax.text((x + right_x) / 2, (y + right_y) / 2, (z + right_z) / 2 + 0.1, '1', color='red')
+            ax.text((x + right_x) / 2, (y + right_y) / 2, (z + right_z) / 2 + 0.03, '1', color='red')
             plot_tree_3d(node.right, right_x, right_y, right_z, ax, depth + 1, branch_length, node_counts)
 
     return ax
 
+
 # Huffman Code algorithm
-text = "MISSISSIPPI"
+# Example text
+text = input("Enter a text to code in Huffman: ")
+
 frequency_table = generate_frequency_table(text)
 root = generate_huffman_tree()
 
 fig = plt.figure(figsize=(10, 5))
 plt.title('3D Binary Tree Visualization')
 
-# Add subplot for the 3D tree visualization
 ax1 = fig.add_subplot(121, projection='3d')
-
-# Plot the Huffman tree in 3D
 ax1 = plot_tree_3d(root, ax=ax1)
 
-# Add subplot for the frequency table
 ax2 = fig.add_subplot(122)
 ax2.axis('off')
 ax2.table(cellText=[[char, freq] for char, freq in frequency_table.items()],
@@ -93,9 +92,3 @@ ax2.table(cellText=[[char, freq] for char, freq in frequency_table.items()],
           loc='center')
 
 plt.show()
-
-
-"""
-singe character words do not work
-MISSISSIPPI does not work 4 different characters
-"""
